70ea36da02f01d0842797724cd5512682604c89d
Voronovich_dev <alexanderbizv1@gmail.com>
2022-09-26 19:06:15 +0300

    feat: implement dot-clicking logic

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const images = document.querySelectorAll('.slider-row img')
 const dots = document.querySelectorAll('.dots-wrapper div')
 const dots_arr = Array.from(dots)
-console.log(dots_arr[0])
 const activeDot = document.querySelector('.dot-active')
 const sliderRow = document.querySelector('.slider-row')
 
@@ -28,33 +27,44 @@ window.addEventListener('resize', () => {
    setDefaultDot()
    setDefaultSlide()
 })
+window.addEventListener('load', () => {
+   removeActiveFromDots()
+   setDefaultDot()
+   setDefaultSlide()
+})
 
+// click next btn
 document.querySelector('.arrow-next').addEventListener('click', () => {
    swapTo('right')
 })
+
+// click prev btn
 document.querySelector('.arrow-prev').addEventListener('click', () => {
    swapTo('left')
 })
 
 
+// ========Slide-switching logic========
 function swapTo(side) {
    const slide_width = returnCurrentSlideWidth()
 
+
+   // offset calculation depending on the pressed button
    offset = side === 'right'
       ? offset + (slide_width + row_gap)
       : offset - (slide_width + row_gap)
-   console.log(offset, 'offset')
 
    const width_swap = side === 'right' ? offset : -offset
-   console.log(width_swap, 'width_swap')
-
 
    const active_dot = document.querySelector('.dot-active')
+
+   //return to the first slide if scrolled to the end
    if (width_swap > (slide_width * images.length) / 2 && -offset !== width_swap) {
       sliderRow.style.left = 0
       offset = -((slide_width * (images.length - 1)) / 2) - ((images.length - 3) * row_gap)
       removeActiveFromDots()
       dots_arr[0].classList.add('dot-active')
+      //return to the last slide if scrolled to the first slide
    } else if (width_swap > (slide_width * images.length) / 2 && offset !== width_swap) {
       sliderRow.style.left = 0
       offset = ((slide_width * (images.length - 1)) / 2) + ((images.length - 3) * row_gap)
@@ -64,7 +74,7 @@ function swapTo(side) {
 
    sliderRow.style.left = -offset + 'px'
 
-
+   // ========Dot-coloring logic========
    const active_dot_index = dots_arr.indexOf(active_dot)
    active_dot.classList.remove('dot-active')
 
@@ -77,4 +87,34 @@ function swapTo(side) {
 }
 
 
+// ========Dot-clicking logic========
+document.querySelector('.dots-wrapper').addEventListener('click', (e) => {
 
+   if (!e.target.classList.contains('dot')) return
+
+   const idexOfClickedDot = dots_arr.indexOf(e.target)
+   const active_dot = document.querySelector('.dot-active')
+
+   const countOfSlides = slidesListed(dots_arr.indexOf(active_dot), idexOfClickedDot)
+
+   const direction = idexOfClickedDot > dots_arr.indexOf(active_dot) ? 'right' : 'left'
+
+   removeActiveFromDots()
+   e.target.classList.add('dot-active')
+
+   const width = (returnCurrentSlideWidth() + row_gap) * countOfSlides
+   navigateByDot(width, direction)
+})
+
+function slidesListed(defaultIndex, clickedIndex) {
+   if (defaultIndex === clickedIndex) return 0
+   return clickedIndex > defaultIndex ? clickedIndex - defaultIndex : defaultIndex - clickedIndex
+}
+
+function navigateByDot(width, direction) {
+   offset = direction === 'right'
+      ? offset + width
+      : offset - width
+
+   sliderRow.style.left = -offset + 'px'
+}
